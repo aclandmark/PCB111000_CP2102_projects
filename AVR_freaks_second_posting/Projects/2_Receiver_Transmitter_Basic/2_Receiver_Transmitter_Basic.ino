43c4ad8d8db00eb574c84b1c3c76e1a6cf626af2
aclandmark <mark.osborne15@btinternet.com>
2023-05-08 10:29:48 +0100

    Update 2_Receiver_Transmitter_Basic.ino

--- AVR_freaks_second_posting/Projects/2_Receiver_Transmitter_Basic/2_Receiver_Transmitter_Basic.ino
+++ AVR_freaks_second_posting/Projects/2_Receiver_Transmitter_Basic/2_Receiver_Transmitter_Basic.ino
@@ -299,16 +299,3 @@ void Num_to_PC_Basic (long number)
 
 
 /********************************************************************************************************/
-/*unsigned int PRN_16bit_GEN(unsigned int start){              //Pseuo random numbrer generation: Google  LFSR for details
-unsigned int bit, lfsr;
-
-if(!(start)) lfsr = (eeprom_read_byte((uint8_t*)(0x3F3)) << 8) 
-+ eeprom_read_byte((uint8_t*)(0x3F2));
-else lfsr = start;
-bit = (( lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
-lfsr = (lfsr >> 1) | (bit << 15);
-if(!(start)){
-eeprom_write_byte((uint8_t*)(0x3F3),(lfsr>>8));
-eeprom_write_byte((uint8_t*)(0x3F2),lfsr);}
-
-return lfsr;}*/

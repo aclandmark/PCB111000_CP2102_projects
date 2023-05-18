4b3d58a785fb33bac38d0237fee9b1f4f9e45147
aclandmark <mark.osborne15@btinternet.com>
2023-05-18 15:10:35 +0100

    Update 6E_FPN_to_display.ino

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6E_FPN_to_display/6E_FPN_to_display.ino
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6E_FPN_to_display/6E_FPN_to_display.ino
@@ -8,32 +8,56 @@ Like 5D but processes floating point data
 
 
 #include "display_FPN_header.h"
-
+#define Buff_Length  20
 
 int main (void){
 float Num_1;
 float power;
-char digits[12];
-
+char digits[Buff_Length + 2];
+char counter = 0;
 
 
 setup_328_HW_Arduino_IO;
 
-
 Serial.write("\r\nEnter scientific number \
-& terminate with Return key.\r\n");
-
+& terminate with Return key.\r\n\
+Press SW3 to pause the display.\r\n\
+SW1 to scroll through the results.\r\n");
 
 Num_1 = Float_KBD_to_display(digits);                     //Data from keyboard
 
 if(Num_1 > 0.0)power = 1.5; else power = 3.0;
 
 while (1){
- 
+Int_Num_to_PC_A(counter,digits, '\t');  
 Sc_Num_to_PC_A(Num_1,1,5 ,'\r');                            //Send number to PC
-waitforkeypress_A();
-Num_1 = pow(Num_1, power);                                  //Do some arithmetic
-float_num_to_display(Num_1);                              //Sends result to the display
-}                                                         //Generates reset if result of arithmetic is too large or small
 
+if (power == 0.5)
+{if (!(counter%5)){float_num_to_display(Num_1);
+while(switch_1_down);}
+
+if ((Num_1 < (1 + 5.0e-3)) && (Num_1 > (1 - 5.0e-3)))break;}
+
+if (power == 3.0)
+{if ((Num_1 < -1.0E20) || (Num_1 > -1.0e-20)){counter = 15; Num_1 *= -1.0; break;}
+  float_num_to_display(Num_1);
+while(switch_1_down);}
+
+Num_1 = pow(Num_1, power);  counter += 1;}                                //Do some arithmetic
+
+
+power = 1.0/power;
+do{
+Int_Num_to_PC_A(counter,digits, '\t'); 
+Sc_Num_to_PC_A(Num_1,1,6 ,'\r');                                            //Send number to PC
+
+if (!(counter%5)){float_num_to_display(Num_1);
+while(switch_1_down);}
+
+Num_1 = pow(Num_1, power);  counter -= 1; }                               //Do some arithmetic
+while(counter+1);
+
+//Num_1 = pow(Num_1, power);                                  //Do some arithmetic
+//float_num_to_display(Num_1);                              //Sends result to the display
+ 
 SW_reset;}

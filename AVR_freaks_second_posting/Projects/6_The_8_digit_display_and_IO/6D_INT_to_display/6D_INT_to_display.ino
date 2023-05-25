b824d8dd633170c1078d9b0e10a556eaaba88f70
aclandmark <mark.osborne15@btinternet.com>
2023-05-25 21:11:52 +0100

    Update 6D_INT_to_display.ino

--- AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6D_INT_to_display/6D_INT_to_display.ino
+++ AVR_freaks_second_posting/Projects/6_The_8_digit_display_and_IO/6D_INT_to_display/6D_INT_to_display.ino
@@ -38,14 +38,14 @@ Note: Zero entry is ignored.\r\n\
 Press SW3 to pause the display\r\n\
 Press reset to repeat.\r\n");                             //Acquires data from keyboard
 
-num_from_KBD = Int_KBD_to_display_Local(digits);
+num_from_KBD = Int_KBD_to_display(digits);
 Arith_mean = (float)num_from_KBD;
 Geom_mean = Arith_mean;
 
 while(1){
 
 while ((switch_1_down) || (switch_2_down) || (switch_3_down))wdr();
-if ((num_from_KBD = Int_KBD_to_display_Local(digits)))
+if ((num_from_KBD = Int_KBD_to_display(digits)))
 
 {Arith_mean = Arith_mean * (float)counter;
 Geom_mean = pow (Geom_mean, (float)counter);

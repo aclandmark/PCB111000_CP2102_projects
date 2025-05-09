59a361ea51048fb8630ded6ca51f8c6691d22bc7
aclandmark <mark.osborne15@btinternet.com>
2025-05-09 17:30:47 +0100

    Update 0_Bitwise_logic.ino

--- pcb_111000_projects_for_CP2102/Arduino Projects/2_binary_and_bit_operations_CP2102/0_Bitwise_logic/0_Bitwise_logic.ino
+++ pcb_111000_projects_for_CP2102/Arduino Projects/2_binary_and_bit_operations_CP2102/0_Bitwise_logic/0_Bitwise_logic.ino
@@ -59,14 +59,6 @@ while (waitforkeypress_Basic() !='x');                                      //Pr
 
 /*************************************************************************************/
 //Type sbroutine "Op" here
-unsigned char Op(unsigned char A, unsigned char B, char comp, char BWOp)
-{char result=0;
-switch (BWOp){
-case '|': result = A | B; break;
-case '^': result = A ^ B; break;
-case '&': result = A & B; break;}
-if (comp == 1) result = ~result;
-return result;}
 
 
 

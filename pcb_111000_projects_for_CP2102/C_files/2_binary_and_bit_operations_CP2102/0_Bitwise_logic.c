bb88a85262bccbfe09fc3639c492c90cf3d1a146
aclandmark <mark.osborne15@btinternet.com>
2025-05-09 17:41:18 +0100

    Create 0_Bitwise_logic.c

---
+++ pcb_111000_projects_for_CP2102/C_files/2_binary_and_bit_operations_CP2102/0_Bitwise_logic.c
@@ -0,0 +1,11 @@
+
+
+unsigned char Op(unsigned char A, unsigned char B, char comp, char BWOp)
+{char result=0;
+switch (BWOp){
+case '|': result = A | B; break;
+case '^': result = A ^ B; break;
+case '&': result = A & B; break;}
+if (comp == 1) result = ~result;
+return result;}
+

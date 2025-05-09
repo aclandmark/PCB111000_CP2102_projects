906918ed0ff310dffc775c417b3abff397245fcc
aclandmark <mark.osborne15@btinternet.com>
2025-05-09 17:49:16 +0100

    Update 3_Char_to_binary.ino

--- pcb_111000_projects_for_CP2102/Arduino Projects/2_binary_and_bit_operations_CP2102/3_Char_to_binary/3_Char_to_binary.ino
+++ pcb_111000_projects_for_CP2102/Arduino Projects/2_binary_and_bit_operations_CP2102/3_Char_to_binary/3_Char_to_binary.ino
@@ -8,30 +8,10 @@ Uses the display to illustrate conversion between 8 bit binary and decimal numbe
 
 #include "Char_to_binary_header.h"
 
-//Type main routine here
-
-
 char digit=1, op, mode;
 
-int main (void){
+//Type main routine here
 
-op=0;
-mode = 'u';                                               //mode: signed or unsigned
-setup_328_HW;
- One_wire_char_as_binary(digit,mode);
-
-set_up_PCI;
-enable_PCI;
-
-sei();
-
-while(1){
-switch(op){
-case 0: break;                                                  //Static display
-case 1:  One_wire_char_as_binary(++digit,mode);break;           //Increment "digit" before calling "I2C_Tx....."
-case 2:  One_wire_char_as_binary(--digit, mode);break;}        //Decrement "digit" before calling "I2C_Tx....."
-
-Timer_T2_10mS_delay_x_m(15);}}
 
 
 

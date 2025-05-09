6f48b6cc44de3ab559cf77be4e4be9e92b266834
aclandmark <mark.osborne15@btinternet.com>
2025-05-09 17:43:02 +0100

    Update 2_Shift_bits.ino

--- pcb_111000_projects_for_CP2102/Arduino Projects/2_binary_and_bit_operations_CP2102/2_Shift_bits/2_Shift_bits.ino
+++ pcb_111000_projects_for_CP2102/Arduino Projects/2_binary_and_bit_operations_CP2102/2_Shift_bits/2_Shift_bits.ino
@@ -84,17 +84,7 @@ lfsr = (PRN_8bit_GEN ());                  //Generate a new PRN (0) tells subrou
 
 /*************************************************************************************/
 //Type subroutine "logical_op" here
-char logical_op(char X, char Y, char op_code) {
-  char result = 0;
-  switch (op_code) {
 
-    case '1': result = Y << X; break;
-    case '2': result = (byte)Y >> X; break;
-    case '3': result = Y | X; break;
-    case '4': result = Y & (~(X)); break;
-    case '5': result = Y ^ X; break;
-    case '6': result = Y & X; break;}
-  return result;}
 
 
 /****************************************************************/

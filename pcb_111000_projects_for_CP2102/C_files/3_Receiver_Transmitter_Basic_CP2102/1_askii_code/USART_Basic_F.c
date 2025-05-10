b444d3541cd0e9e20f22057848e9908ed6f0d717
aclandmark <mark.osborne15@btinternet.com>
2025-05-10 21:24:17 +0100

    Create USART_Basic_F.c

---
+++ pcb_111000_projects_for_CP2102/C_files/3_Receiver_Transmitter_Basic_CP2102/1_askii_code/USART_Basic_F.c
@@ -0,0 +1,36 @@
+#define Array_size 300  
+
+int main (void)          //Example 10
+  { char keypress;
+  int line_counter;
+   char char_store [Array_size];
+  int char_store_pointer;
+   char print_cntrl;
+   
+    setup_328_HW;
+
+  line_counter = 0;
+  char_store_pointer = 0;
+  print_cntrl = 0;
+
+    if(reset_status != 2)
+    {String_to_PC_Basic("Send hex file");
+    while (!(isCharavailable_Local(65)));}
+  else
+    {while (!(isCharavailable_Local(65))) 
+    Char_to_PC_Local('?');}  
+  
+  newline_Basic();
+  Char_to_PC_Local(Char_from_PC_Local());
+  while (1)
+  {if (!(isCharavailable_Local(10)))break;
+        
+      {keypress = Char_from_PC_Local();
+      Char_to_PC_Local(keypress);
+      if (keypress == '\n') line_counter += 1;
+      if (line_counter ==10)UCSR0B &= (~(1<< TXEN0));
+     char_store[char_store_pointer++] = keypress; 
+      char_store_pointer %= Array_size;}}
+      
+  UCSR0B |= (1<< TXEN0);
+  newline_Basic(); 

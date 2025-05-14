d1f24414e1293396df2f397c3a377a24496a6680
aclandmark <mark.osborne15@btinternet.com>
2025-05-14 15:51:54 +0100

    Update Basic_Rx_Tx_Extra.c

--- pcb_111000_projects_for_CP2102/Resource_CP2102_projects/PC_comms/Basic_Rx_Tx_Extra.c
+++ pcb_111000_projects_for_CP2102/Resource_CP2102_projects/PC_comms/Basic_Rx_Tx_Extra.c
@@ -7,39 +7,51 @@ void Long_Hex_and_Int_to_PC_Basic (char, long);
 
 
 void Num_string_to_display (char * display_buffer, char cr_keypress )
- {Int_num_string_to_display; }
+ {Int_num_string_to_display; }							//See "Chip2chip_comms\Display_driver_header.h"
 
 
 
 /****************************************************************************************************************************************************/
-long Int_from_PC_Basic(char digits[]){
+long Int_KBD_to_display(char digits[]){              	//Acquires an integer string from the keyboard and returns the binary equivalent
 char keypress;
-char cr_keypress = 0;
+char cr_keypress = 0;									//Set to one when carriage return keypress terminates the string
 unsigned char num_byte[4];
-long Long_Num_from_mini_OS;
+long Long_Num_from_mini_OS;								//Mini-OS converts string to binary and returns the result as four 8 bit character numbers
 
-for(int n = 0; n<=7; n++) digits[n] = 0; 
-
-do
-{keypress =  waitforkeypress_Basic();} 
-while (!(decimal_digit_Basic(keypress)));                                //(non_decimal_char(keypress));  //Not -,0,1,2,3,4,5,6,7,8 or 9
-digits[0] = keypress;
-Num_string_to_display(digits, cr_keypress);
+for(int n = 0; n<=8; n++) digits[n] = 0;           		//Clear the buffer used for the string
 
 while(1){
-if ((keypress = wait_for_return_key_Basic())  =='\r')break;
-if (decimal_digit_Basic (keypress))                                      //012345678or9  :Builds up the number one keypress at a time
-{for(int n = 7; n>=1; n--)
-digits[n] = digits[n-1];                                                 //Shifts display left for each keypress
+keypress = waitforkeypress_Basic();
+if ((!(decimal_digit(keypress)))
+&& (keypress != '-'))continue;                            //Ignore keypress if it is not OK
 digits[0] = keypress;
-Num_string_to_display(digits, cr_keypress);}}
+break;}
+ 
+Num_string_to_display(digits, cr_keypress);					
+while(1){
+if ((keypress = wait_for_return_key_Basic())  =='\r')break;     //Detect return key press (i.e \r or\r\n)
+if ((decimal_digit(keypress)) || (keypress == '\b')\
+ || (keypress == '-'))
 
-cr_keypress = 1;                                            			//End of string; return key press detected
+{if (keypress == '\b'){                         				//Backspace key
+for (int n = 0; n < 7; n++)
+digits[n] = digits[n + 1];
+digits[7] = 0;}
+
+else
+{for(int n = 8; n>=1; n--)                                    	//Shift display for each new keypress
+digits[n] = digits[n-1];
+digits[0] = keypress;  }                             			//Add new keypress           
+
+Num_string_to_display(digits, cr_keypress);}}                   //Update display includes "cr_keypress"                                                 
+cr_keypress = 1;                                            	//End of string; return key press detected
+
 Num_string_to_display(digits, cr_keypress);
-
 cr_keypress = 0;
- Int_from_mini_OS;
- Long_Hex_and_Int_to_PC_Basic(10,Long_Num_from_mini_OS);
+
+Int_from_mini_OS;												//See "Chip2chip_comms\Display_driver_header.h"
+Long_Hex_and_Int_to_PC_Basic(10,Long_Num_from_mini_OS);
+
 return Long_Num_from_mini_OS;}
 
 

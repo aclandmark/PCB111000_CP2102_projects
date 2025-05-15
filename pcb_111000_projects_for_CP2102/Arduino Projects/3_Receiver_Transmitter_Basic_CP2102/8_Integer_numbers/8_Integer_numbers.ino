3d452a18eb89fbc0e9ad0e4fd116787fdb43aba3
aclandmark <mark.osborne15@btinternet.com>
2025-05-15 17:39:49 +0100

    Update 8_Integer_numbers.ino

--- pcb_111000_projects_for_CP2102/Arduino Projects/3_Receiver_Transmitter_Basic_CP2102/8_Integer_numbers/8_Integer_numbers.ino
+++ pcb_111000_projects_for_CP2102/Arduino Projects/3_Receiver_Transmitter_Basic_CP2102/8_Integer_numbers/8_Integer_numbers.ino
@@ -13,29 +13,7 @@
 
 
 //Type main routine here
-int main (void)  
-  { 
-    char num_string[12];
-    long  num;
-    long m = 1;
- 
- setup_328_HW;
 
-if(reset_status != 2)String_to_PC_Basic(message_1);
-else String_to_PC_Basic(message_2);
-  
-num =  Int_KBD_to_display(num_string);
-
-newline_Basic();
-Int_to_PC_Basic(num);
-String_to_PC_Basic("\t");
-
-do{
-if(!(num%m)){Int_to_PC_Basic(m);}
-m += 1; if(!(m%10000))String_to_PC_Basic(".");}while(m < num);
-
-SW_reset;
-return 1;  }
 
 
 

f683204aa55db94aaeb5cf1f6470d1904e3221fb
aclandmark <mark.osborne15@btinternet.com>
2025-05-17 21:58:03 +0100

    Update 5_Moving_display.c

--- pcb_111000_projects_for_CP2102/C_files/4_Display_Driver_Basic_CP2102/5_Moving_display.c
+++ pcb_111000_projects_for_CP2102/C_files/4_Display_Driver_Basic_CP2102/5_Moving_display.c
@@ -8,21 +8,24 @@ int main (void){
   int p;
   
 setup_328_HW;
+
+One_wire_comms_any_segment_clear_all();
+
 if(reset_status == 2)String_to_PC_Basic("\r\nAgain"); else
-String_to_PC_Basic("Enter number at keyboard\r\n");
+String_to_PC_Basic("\r\nEnter number at keyboard\r\n");
 num_length = Int_KBD_to_display_Local(num_string);
 
-String_to_PC_Basic(num_string);
 One_wire_comms_any_segment_clear_all();
 for(int m = 0; m <=15; m++)display_string[m] = 0;
 
 p = 0;
 while(1){
 display_string[0] = num_string[num_length - p%(num_length+1)]; p += 1;
-Num_string_to_display (display_string, 0 );_delay_ms(100);
+Num_string_to_display (display_string, 0 );SW_Delay_ms(100);
 
 for(int m = 0; m < 15; m++){
 display_string[15-m] = display_string[15-m-1];}
 
 if(switch_1_down) break;}
+while(switch_1_down);
 SW_reset;}

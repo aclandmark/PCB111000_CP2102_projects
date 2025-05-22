8f66b473e465ab1330bf63552a79dbba0dfa0eea
aclandmark <mark.osborne15@btinternet.com>
2025-05-22 14:31:40 +0100

    Update 5_File_download_with_interrupt.ino

--- pcb_111000_projects_for_CP2102/Arduino Projects/5_Interrupts_CP2102/5_File_download_with_interrupt/5_File_download_with_interrupt.ino
+++ pcb_111000_projects_for_CP2102/Arduino Projects/5_Interrupts_CP2102/5_File_download_with_interrupt/5_File_download_with_interrupt.ino
@@ -4,39 +4,28 @@
 To make this work open file "Resource_CP2102_projects\Chip2chip_comms\One_wire_transactions_1.c
 
 Find subroutine ISR(PCINT0_vect) and enable global interrupts as follows: "ISR(PCINT0_vect){ sei(); ........."
- 
+
  */
 
 
 
 #include "File_download_with_interrupt_header.h"
 
+
 #define ctlz  0x1a
+#define halt_reset_switch       PCMSK1 &= (~(1 << PCINT13));
+#define restart_reset_switch    PCMSK1 |= (1 << PCINT13);
+
 
 volatile long PORT_1 = 1;  
 volatile char download_complete = 0;
 char symbol;
 
+
+
 //Type main routine here
+ 
 
-int main (void)   
-  {setup_328_HW;
-if(reset_status != 2)
-String_to_PC_Basic("Send file: Ctrl-Z when finished\r\n");
-
-  
-while (!(isCharavailable_Basic(32)))
-    Char_to_PC_Basic('?');  
-  newline_Basic();
-Char_to_PC_Basic(Char_from_PC_Basic());
-T1_clock_tick(100);
-
-UCSR0B |= (1 << RXCIE0);while(!(download_complete));
-
-String_to_PC_Basic("\r\nDone: AK to repeat\r\n");
-cli();
-waitforkeypress_Basic();
-SW_reset;}
 
 
 
@@ -53,18 +42,21 @@ OCR1A = T1_period_in_ms * 125;
 
 /**************************************************************************************************************/
 ISR(TIMER1_COMPA_vect) 
-{ TCNT1 = 0; sei();
+{ TCNT1 = 0; sei(); 
  if(PORT_1 < 0x10000)One_wire_Tx_2_integers(PORT_1, PORT_1);
 PORT_1 = PORT_1 << 1;
-if (PORT_1 == 0x10000)PORT_1 = 1;}
+if (PORT_1 == 0x10000)PORT_1 = 1;
+}
 
 
 
 /**************************************************************************************************************/
-ISR(USART_RX_vect){ 
+ISR(USART_RX_vect){
+
 symbol = Char_from_PC_Basic();
 if(symbol != ctlz)Char_to_PC_Basic(symbol);
-else download_complete = 1;}
+else download_complete = 1;
+}
 
 
 

c7b68319c55e245afd5cba16c7c2a15640612bd2
aclandmark <mark.osborne15@btinternet.com>
2025-05-22 14:28:37 +0100

    Update 5_File_download_with_interrupt.c

--- pcb_111000_projects_for_CP2102/C_files/5_Interrupts_CP2102/5_File_download_with_interrupt.c
+++ pcb_111000_projects_for_CP2102/C_files/5_Interrupts_CP2102/5_File_download_with_interrupt.c
@@ -2,7 +2,7 @@
 
 
 
- int main (void)   
+int main (void)   
   {setup_328_HW;
 if(reset_status != 2)
 String_to_PC_Basic("Send file: Ctrl-Z when finished\r\n");
@@ -12,11 +12,14 @@ while (!(isCharavailable_Basic(32)))
     Char_to_PC_Basic('?');  
   newline_Basic();
 Char_to_PC_Basic(Char_from_PC_Basic());
+halt_reset_switch ;
 T1_clock_tick(100);
 
 UCSR0B |= (1 << RXCIE0);while(!(download_complete));
 
-String_to_PC_Basic("\r\nDone: AK to repeat\r\n");
+restart_reset_switch;
+
+String_to_PC_Basic("\r\nDone: Press AK to repeat or reprogram\r\n");
 cli();
 waitforkeypress_Basic();
 SW_reset;}

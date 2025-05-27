a90cb801c0e55b7268dbc155d0786445bf5b601d
aclandmark <mark.osborne15@btinternet.com>
2025-05-27 18:02:25 +0100

    Update E_Prime_numbers_extra.c

--- pcb_111000_projects_for_CP2102/C_files/6_Prime_numbers/E_Prime_numbers_extra.c
+++ pcb_111000_projects_for_CP2102/C_files/6_Prime_numbers/E_Prime_numbers_extra.c
@@ -6,13 +6,13 @@ ISR(PCINT2_vect){
 if((switch_1_up) &&(switch_3_up)) return; 
 
 if(switch_1_down){_delay_ms(10);
-while(switch_1_down);return;}           								//Press sw_1 to pause display
+while(switch_1_down);return;}                           //Press sw_1 to pause display
 
 if(switch_3_down)
 {while (switch_3_down){start_1+=2; 
-Int_to_PC_Basic(start_1);        										//Hold sw_3 down to increment start point
+Int_to_PC_Basic(start_1);                           //Hold sw_3 down to increment start point
 Char_to_PC_Basic(' ');
-Timer_T1_sub(T1_delay_1sec);}                   						//for random number generator
+Timer_T1_sub(T1_delay_1sec);}                               //for random number generator
 newline_Basic(); 
 return;}
 }
@@ -21,8 +21,10 @@ return;}
 /***************************************************************************************************************************************************/
 void factors_to_display(int * factor, int m){
   
-  for(int n=0; n<m; n++){I2C_Tx_any_segment_clear_all();
-  Timer_T0_10mS_delay_x_m(10);
-  I2C_Tx_long(factor [n]);
+  void factors_to_display(int * factor, int m){
+  
+  for(int n=0; n<m; n++){One_wire_comms_any_segment_clear_all();
+  Timer_T2_10mS_delay_x_m(10);
+ Int_num_to_display(factor [n]);
   while (switch_2_up)wdr();
   while (switch_2_down)wdr();}}

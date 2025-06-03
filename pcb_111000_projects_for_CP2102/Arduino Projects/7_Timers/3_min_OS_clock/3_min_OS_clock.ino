25dd03861dc1b7c9294d22773e9f58f4e6700802
aclandmark <mark.osborne15@btinternet.com>
2025-06-03 12:44:56 +0100

    Update 3_min_OS_clock.ino

--- pcb_111000_projects_for_CP2102/Arduino Projects/7_Timers/3_min_OS_clock/3_min_OS_clock.ino
+++ pcb_111000_projects_for_CP2102/Arduino Projects/7_Timers/3_min_OS_clock/3_min_OS_clock.ino
@@ -63,29 +63,9 @@ clear_display;
 _delay_ms(50);}
 
 
+//Tyope subroutines "void initialise_T2_Local(void)", "void start_clock_Local(void)"
+//and "ISR (TIMER2_COMPA_vect)" here
 
-
-/****************************************************************************************************************/
-void initialise_T2_Local(void){
-ASSR = (1 << AS2); 
-TCNT2 = 0;
-TCCR2A = 0;
-TCCR2B |= (1 << CS20) | (1 << CS21);
-OCR2B = 0;}
-
-
-void start_clock_Local(void){
-tick_counter = 0;
-TCNT2 = 0;
-OCR2A = 102; 
-TIMSK2 |= (1 << OCIE2A);}
-
-
-ISR (TIMER2_COMPA_vect){ char string[5];
-  OCR2A += 102;
-  clock_tick += 1;
-  tick_counter += 1;
-  if(tick_counter == 9){tick_counter = -1; OCR2A += 4;}}
   
 
 /************************************************************************************************************/

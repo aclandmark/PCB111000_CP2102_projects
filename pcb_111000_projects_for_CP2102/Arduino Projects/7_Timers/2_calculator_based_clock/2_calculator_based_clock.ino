bd758606656a14189191493e2bc9e890a8735dc0
aclandmark <mark.osborne15@btinternet.com>
2025-06-03 12:45:03 +0100

    Update 2_calculator_based_clock.ino

--- pcb_111000_projects_for_CP2102/Arduino Projects/7_Timers/2_calculator_based_clock/2_calculator_based_clock.ino
+++ pcb_111000_projects_for_CP2102/Arduino Projects/7_Timers/2_calculator_based_clock/2_calculator_based_clock.ino
@@ -45,16 +45,7 @@ Format_time_for_display();Display_time;}}
 
 
 /**********************************************************************************************************************/
-void Format_time_for_display(void){
-Hours =     deci_sec_counter/36000;
-Minutes =   (deci_sec_counter%36000)/600;
-Seconds =   ((deci_sec_counter%36000)%600)/10;
-deci_Secs = ((deci_sec_counter%36000)%600)%10;
-
-timer_utoa(Hours); HoursH = charH; HoursL = charL; 
-timer_utoa(Minutes); MinsH = charH; MinsL = charL; 
-timer_utoa(Seconds); SecsH = charH; SecsL = charL; 
-timer_utoa(deci_Secs * 10); deci_SecsH = charH; deci_SecsL = charL; }
+//Type subroutines "void Format_time_for_display(void)" and "void timer_utoa(char n)" here
 
 
 
@@ -82,17 +73,6 @@ deci_sec_counter = 10*(long)((((long)((HoursH -'0') * 10) + HoursL-'0') * 3600) 
 clear_display;
 _delay_ms(50);}
 
-
-
-/**********************************************************************************************************************/
-void timer_utoa(char n){                //converts an unsigned number to a string of digits
-if (n>=10)
-{charL = n%10;
-charH = (n-charL)/10;
-charL = charL + '0';
-charH = charH + '0';}
-else
-{charH = '0'; charL = n + '0';}}
 
 
 

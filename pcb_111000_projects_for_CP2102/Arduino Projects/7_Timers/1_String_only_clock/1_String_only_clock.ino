a0b50a54d6e590d6f32256504322758a2ab7c068
aclandmark <mark.osborne15@btinternet.com>
2025-06-03 12:45:09 +0100

    Update 1_String_only_clock.ino

--- pcb_111000_projects_for_CP2102/Arduino Projects/7_Timers/1_String_only_clock/1_String_only_clock.ino
+++ pcb_111000_projects_for_CP2102/Arduino Projects/7_Timers/1_String_only_clock/1_String_only_clock.ino
@@ -40,22 +40,10 @@ while(1){while(clock_tick <= 1);clock_tick = 0; Inc_time();Inc_time();Display_ti
 
 
 /****************************************************************************************************************/
-void Inc_time(void){
- if (deci_SecsH < '9') deci_SecsH++; 
-  else {deci_SecsH = '0'; if ((SecsL) < '9') SecsL++;   
-              else {SecsL = '0'; if (SecsH < '5') SecsH++; 
-                          else {SecsH = '0';  if ((MinsL) < '9') MinsL++; 
-                                      else {MinsL = '0'; if (MinsH < '5') MinsH++; else {MinsH = '0';
+//Type subroutine "void Inc_time(void)" here
 
- switch (HoursH){ case '0': case '1': if ((HoursL & 0x7F) < '9')HoursL++; else {HoursL='0'; HoursH++;} break;
- case '2': if((HoursL) < '3')HoursL++; else {HoursL='0'; HoursH='0';} break;}
 
- }  //  update deci_Secs, seconds units and tens, minutes units and tens and hours
- }  //  update deci_Secs, seconds units and tens and minutes units and tens
- }  //  update deci_Secs, seconds units and tens and minutes units
- }  //  update deci_Secs and seconds units and tens
- }  //  update deci_Secs and seconds units
-}
+
 
 
 

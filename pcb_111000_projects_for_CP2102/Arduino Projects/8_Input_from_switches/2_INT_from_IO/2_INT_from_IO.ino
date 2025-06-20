69d513041b1fc5806e6e2c40e7b11fc38c7c89a8
aclandmark <mark.osborne15@btinternet.com>
2025-06-20 16:59:52 +0100

    Update 2_INT_from_IO.ino

--- pcb_111000_projects_for_CP2102/Arduino Projects/8_Input_from_switches/2_INT_from_IO/2_INT_from_IO.ino
+++ pcb_111000_projects_for_CP2102/Arduino Projects/8_Input_from_switches/2_INT_from_IO/2_INT_from_IO.ino
@@ -1,20 +1,5 @@
 
-/*
-Takes an integer number from the IO, converts it to floating point
-and then to the form A * 2^N where A is between 1 and 2 and N is an integer.
 
-Press 
-  SW1 to scroll through the digits
-  SW2 to shift the display left
-  SW3 to enter the number
-
-Then press SW1 and SW3 to display A and N
-
-Switch assignments:
-    SW1 PORTD2 PCINT18 
-    SW2 PORTD5 PCINT21
-    SW3 PORTD7 PCINT23
-*/
 
  
 #include "INT_from_IO_header.h"
@@ -23,24 +8,24 @@ Switch assignments:
 int main (void){
   
 long Num_1;
-char digits[8];
 int counter = 0;
 
 
 setup_328_HW;
 
-Serial.write("\r\nDATA FROM I/O");
-Serial.write(message_1);
+String_to_PC_Basic("\r\nDATA FROM I/O");
+String_to_PC_Basic(message_1);
 
-while((switch_1_down) || (switch_2_down) ||(switch_3_down));        //wait for switch release
-
+wait_for_switch_release;
 Data_Entry_complete=0;
 clear_display;                    
 Num_1 = Int_number_from_IO();
 
 do{
-Serial.print(++counter); Serial.write('\t');
-Serial.print(Num_1); Serial.write("\r\n");
+  while(switch_2_down)wdr();
+Int_to_PC_Basic(++counter); Char_to_PC_Basic('\t');
+Int_to_PC_Basic(Num_1);
+String_to_PC_Basic("\r\n");
 
 Int_num_to_display(Num_1);                                           //Sends number to the display
 SW_Delay_ms(100);
@@ -54,9 +39,9 @@ while(switch_1_up)wdr();
 Num_1 = (Num_1 / 3) *2; 
 
 do{Num_1 = (Num_1 / 3) *2;                                           //Do the arithmetic in reverse
-Serial.print(--counter); Serial.write('\t');
-Serial.print(Num_1); 
-Serial.write("\r\n");                                             
+Int_to_PC_Basic(--counter); Char_to_PC_Basic('\t');
+Int_to_PC_Basic(Num_1); 
+newline_Basic();                                           
 Int_num_to_display(Num_1);
 SW_Delay_ms(100);
 while(switch_1_down)wdr();}while (counter-1);
@@ -72,10 +57,7 @@ SW_reset;}
 ISR(PCINT2_vect){
 char disp_bkp[8];
 
-if((switch_1_up) && (switch_2_up) && (switch_3_up))return;
-if ((switch_2_down) && (switch_3_down))
-{while((switch_2_down) || (switch_3_down))wdr(); return;}        //Unwanted switch presses
-
+Ignore_Unwanted_pin_change_interrupts\
 
 if(switch_3_down){                                              //SW3 is used to terminate data entry
 digit_entry = 1;                                                //It is also used to generate decimal point
@@ -89,7 +71,7 @@ while(switch_1_down)
 {scroll_int_display_zero(); 
 Timer_T2_10mS_delay_x_m(10);}
 
-while(switch_3_down)wdr();
+//while(switch_3_down)wdr();
 enable_PCI_on_sw3;  
 
 if(switch_2_down)shift_int_display_left();  

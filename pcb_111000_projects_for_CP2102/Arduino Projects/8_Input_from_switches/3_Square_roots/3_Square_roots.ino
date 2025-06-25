90993e5cdd98f5364fd85c5fa5ecaf2d1b45afca
aclandmark <mark.osborne15@btinternet.com>
2025-06-25 16:24:05 +0100

    Update 3_Square_roots.ino

--- pcb_111000_projects_for_CP2102/Arduino Projects/8_Input_from_switches/3_Square_roots/3_Square_roots.ino
+++ pcb_111000_projects_for_CP2102/Arduino Projects/8_Input_from_switches/3_Square_roots/3_Square_roots.ino
@@ -5,43 +5,7 @@
 #include "square_root_subroutines.c"
 
 
-
-
-
 //Enter the main routine here
-int main (void){
-long Num_1;
-char result[9];
-int p;
-
-setup_328_HW;
-
-
-if(reset_status == 2)String_to_PC_Basic(message_2);
-else String_to_PC_Basic(message_1);
-
-while((switch_1_down) || (switch_2_down) ||(switch_3_down));        //wait for switch release
-
-Num_1 = Int_number_from_IO();
-Timer_T2_10mS_delay_x_m(15);
-
-root_computation(Num_1, display_buffer);
-    String_to_PC_Basic(display_buffer); 
-    for(int m = 0; m < 9; m++)result[m] = display_buffer[8-m];
-
-for(int m = 8; m >= 1; m--)if(result[m-1] == '.') 
-{result [m] |= 0x80 ; p = m; break;}
-
-for ( int m = p; m >= 1; m--) result[m-1] = result[m-2];
-
-   for(int m = 0; m<=7; m++)result[0+m] = result[1+m];
-
-   
-   Num_string_to_display(result,0);          
-
-while(switch_3_up);
-SW_reset;}
-
 
 
 

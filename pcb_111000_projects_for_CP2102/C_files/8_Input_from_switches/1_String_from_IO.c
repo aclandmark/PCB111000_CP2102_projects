aeb5d5650eb50c4c199ea525498068be74bc3122
aclandmark <mark.osborne15@btinternet.com>
2025-07-02 16:49:27 +0100

    Update 1_String_from_IO.c

--- pcb_111000_projects_for_CP2102/C_files/8_Input_from_switches/1_String_from_IO.c
+++ pcb_111000_projects_for_CP2102/C_files/8_Input_from_switches/1_String_from_IO.c
@@ -1,12 +1,12 @@
-
-
-
 int main (void){
 
-setup_328_HW;
+int counter; 
+setup_328_HW_A;
 
+if(reset_status != 2){
 Serial.write("\r\nDATA FROM I/O");
-Serial.write(message_1);
+Serial.write(message_1);}
+else Serial.write("\r\nAgain\r\n");
 
 wait_for_switch_release;
 clear_display;                    
@@ -17,10 +17,10 @@ initialise_display;
 
 do{
 while (!(digit_entry))wdr();            //Wait for user to select the next digit
-digit_entry = 0;                       //SW2 sets this to one
-}while(!(Data_Entry_complete));        //Remain in do-loop until data entry has been terminated
+digit_entry = 0;}                       //SW2 sets this to one
+while(!(Data_Entry_complete));        //Remain in do-loop until data entry has been terminated
 
 rotate_display();
+while(switch_1_down)wdr();
 
 SW_reset;}
-

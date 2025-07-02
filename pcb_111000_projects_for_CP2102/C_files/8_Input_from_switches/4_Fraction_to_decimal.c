07a645d41c9f17c99666bc78479a0f048202af94
aclandmark <mark.osborne15@btinternet.com>
2025-07-02 16:49:43 +0100

    Update 4_Fraction_to_decimal.c

--- pcb_111000_projects_for_CP2102/C_files/8_Input_from_switches/4_Fraction_to_decimal.c
+++ pcb_111000_projects_for_CP2102/C_files/8_Input_from_switches/4_Fraction_to_decimal.c
@@ -1,23 +1,23 @@
 
 
-
 int main (void){
 long Num_1, Num_2;
+char num_string[12];
 
-setup_328_HW;
+setup_328_HW_A;
 
 if (reset_status != 2)
-String_to_PC_Basic("\r\nNum_1 and Num_2 ? (Num_2 > Num_1)\r\n\r\n");
-else String_to_PC_Basic("\r\nAgain\r\n\r\n");
+Serial.write("\r\nNum_1 and Num_2 ? (Num_2 > Num_1)\r\n\r\n");
+else Serial.write("\r\nAgain\r\n\r\n");
 
 Num_1 = Int_number_from_IO();     
 Num_2 = Int_number_from_IO();
 
-Int_to_PC_Basic (Num_1); 
-String_to_PC_Basic("  divided by  ");
+Int_Num_to_PC_A(Num_1, num_string, '\t'); 
+Serial.write("  divided by  ");
 Int_to_PC_Basic (Num_2);
-String_to_PC_Basic("  equals\r\n");
+Serial.write("  equals\r\n");
 
 fraction_to_decimal_string_Local(Num_1, Num_2, display_buffer); 
-newline_Basic();
+Serial.write("\r\n");
 SW_reset;}

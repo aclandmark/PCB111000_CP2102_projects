e32600b6cb510ef3e8802c78cf2173e52a865adf
aclandmark <mark.osborne15@btinternet.com>
2025-07-02 16:45:07 +0100

    Update Local_subroutines.c

--- pcb_111000_projects_for_CP2102/Arduino Projects/8_Input_from_switches/1_String_from_IO/Local_subroutines.c
+++ pcb_111000_projects_for_CP2102/Arduino Projects/8_Input_from_switches/1_String_from_IO/Local_subroutines.c
@@ -1,5 +1,5 @@
 
-
+#include <avr/wdt.h>
 #include <avr/io.h>
 #include <stdio.h>
 #include <util/delay.h>
@@ -7,11 +7,12 @@
 
 #define disable_PCI_local            PCMSK2 &= (~((1 << PCINT18) | (1 << PCINT21) | (1 << PCINT23)));
 #define switch_1_down_local         (PIND & 0x04)^0x04
+#define switch_2_down_local         (PIND & 0x20)^0x20
 
 
 void One_wire_comms_any_segment_clear_all(void);
 void Num_string_to_display(char *, char);
-
+void SW_Delay_ms(int);
 
 extern char display_buffer[8];
 
@@ -23,6 +24,7 @@ void rotate_display(){
   char display_string[8];
   char num_length = 0;
   int p;
+  
 
  disable_PCI_local;
 
@@ -34,11 +36,15 @@ One_wire_comms_any_segment_clear_all();
 for(int m = 0; m <=7; m++)display_string[m] = 0;
 p = 0;
 while(1){
-display_string[0] = display_buffer[num_length - p%(num_length+1)]; p += 1;
-Num_string_to_display(display_string,0);_delay_ms(50);
+display_string[0] = display_buffer[num_length - p%(num_length+1)]; 
+p += 1;
+Num_string_to_display(display_string,0);
+SW_Delay_ms(100);
 
 for(int m = 0; m < 7; m++){
 display_string[7-m] = display_string[7-m-1];}
+
+while(switch_2_down_local)wdr();
 
 if(switch_1_down_local) break; }}
 

a707dbc1d616f1c66d1e71bc1797786dc8120bf7
aclandmark <mark.osborne15@btinternet.com>
2025-08-08 15:51:11 +0100

    Update Local_subroutines.c

--- pcb_111000_projects_for_CP2102/Arduino Projects/9_Real_numbers/B_FPN_to_String/Local_subroutines.c
+++ pcb_111000_projects_for_CP2102/Arduino Projects/9_Real_numbers/B_FPN_to_String/Local_subroutines.c
@@ -11,7 +11,7 @@ void real_divide(long, long, long *, long *);
 
 
 
-long Int_KBD_to_display_Local(char digits[], long* divisor, char * decimal_places){    
+long Real_Num_from_PC_local(char digits[], long* divisor, char * decimal_places){    
 char keypress;
 int digit_counter = 0;
 newline_Basic();

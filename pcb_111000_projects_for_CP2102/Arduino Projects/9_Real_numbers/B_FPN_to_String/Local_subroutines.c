4a268b686f0b419b6d3324b2338bc0e9ef543ecf
aclandmark <mark.osborne15@btinternet.com>
2025-08-14 16:28:55 +0100

    Update Local_subroutines.c

--- pcb_111000_projects_for_CP2102/Arduino Projects/9_Real_numbers/B_FPN_to_String/Local_subroutines.c
+++ pcb_111000_projects_for_CP2102/Arduino Projects/9_Real_numbers/B_FPN_to_String/Local_subroutines.c
@@ -11,25 +11,9 @@ void real_divide(long, long, long *, long *);
 
 
 
-long Real_Num_from_PC_local(char digits[], long* divisor, char * decimal_places){    
-char keypress;
-int digit_counter = 0;
-newline_Basic();
 
-*divisor = 0;
-*decimal_places = 0;
-while(1){
-if ((keypress = wait_for_return_key_Basic())  =='\r')break; 
-if (keypress == '.'){*divisor = 1; Char_to_PC_Basic('.');}
-if (decimal_digit_Basic(keypress))
-{ Char_to_PC_Basic(keypress);
-  digits[digit_counter++] = keypress;
-if (*divisor) {*divisor *= 10;
-*decimal_places += 1;}}}
-if(!(*divisor))*divisor = 1;
-digits[digit_counter] = '\0' ;                                                 
-return atol (digits);}
-
+//Type subroutine long Real_Num_from_PC_local(char digits[], long* divisor, char * decimal_places){}
+//here
 
 
 

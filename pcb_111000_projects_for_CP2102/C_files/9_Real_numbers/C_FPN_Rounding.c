c6220c3c407835d9fc44545a8d573fc12e3d2546
aclandmark <mark.osborne15@btinternet.com>
2025-08-14 16:28:08 +0100

    Create C_FPN_Rounding.c

---
+++ pcb_111000_projects_for_CP2102/C_files/9_Real_numbers/C_FPN_Rounding.c
@@ -0,0 +1,28 @@
+
+
+void Real_num_to_PC_no_rounding(long int_num, int twos_expt, char decimal_places){
+long  divisor;
+long Div, mod;
+
+
+Char_to_PC_Basic('\t');
+ 
+ if (twos_expt < 0){
+ divisor = 1;
+ twos_expt = twos_expt * (-1);
+
+ while (twos_expt >= 31){twos_expt -= 1; int_num = int_num >> 1;}
+ for (int m = 0; m < twos_expt; m++) divisor *= 2;
+
+ real_divide(int_num, divisor, &Div, &mod); 
+ Int_to_PC_Basic(Div);
+ Char_to_PC_Basic('.');
+ for(int m = 0; m < decimal_places; m++){
+
+ while (mod >= 0x7FFFFFFF/10){mod = mod/10; divisor /= 10;}
+  
+ real_divide(mod*10, divisor, &Div, &mod);
+ Char_to_PC_Basic(Div + '0');}}
+
+
+else {while(twos_expt) {int_num *= 2; twos_expt -= 1;}Int_to_PC_Basic(int_num);}}

69b8a9540518694796a52ed5eb77569ae54d3c50
aclandmark <mark.osborne15@btinternet.com>
2025-08-14 15:55:18 +0100

    Update D_FPN_Multiplication.ino

--- pcb_111000_projects_for_CP2102/Arduino Projects/9_Real_numbers/D_FPN_Multiplication/D_FPN_Multiplication.ino
+++ pcb_111000_projects_for_CP2102/Arduino Projects/9_Real_numbers/D_FPN_Multiplication/D_FPN_Multiplication.ino
@@ -1,10 +1,15 @@
 
-     
+    /*
+  For multiplication each number is converted to a 24 bit integer plus twos exponent
+  The integers are then multiplied and the exponents added.
+  The main complication is that multiplication of 24 bit numbers produces a 48 bit result which requirs 2 long numbers
+For integer numbers the product must be less than (256^4)/2 to avoid overflow in the printing process.
+  
+*/
 
-//https://www.h-schmidt.net/FloatConverter/IEEE754.html
 
 
-
+  
 
 #include <string.h>
 
@@ -30,16 +35,7 @@ float fpn_result;
 
 setup_328_HW;
 
-/*waitforkeypress_Basic();
-Num[0] = 125;
-Num[1] = 57;
-newline_Basic();
-Product_Low = Int_multiplier_24_bit(Num[0], Num[1]);
-Int_to_PC_Basic(Product_Low);
-print_long_as_binary(Product_Low, '\r');*/
-
 if (reset_status != 2)String_to_PC_Basic("\r\nEnter two real numbers to be multiplied together \r\n");
-
 
 while(1){
 for(int p = 0; p < 2; p++){
@@ -47,7 +43,6 @@ Num[p] = Real_Num_from_PC_local(digits, &tens_expt, &decimal_places);
 String_to_PC_Basic("\t=\t");
 
 Num[p] = int_divide (Num[p], tens_expt, &twos_expt[p]);    
- //print_long_as_binary(Num[p], '\t');
  Int_to_PC_Basic(Num[p]);
  String_to_PC_Basic(" times 2^");Hex_and_Int_to_PC_Basic(10, twos_expt[p]);}
 
@@ -59,7 +54,7 @@ Int_to_PC_Basic(Product);String_to_PC_Basic(" times 2^");Hex_and_Int_to_PC_Basic
 String_to_PC_Basic("\tNum decimal places? "); decimal_places = waitforkeypress_Basic() - '0';
 Char_to_PC_Basic(decimal_places + '0'); 
 
-Real_num_to_PC_no_rounding(Product,twos_expt[2], decimal_places + 1);Char_to_PC_Basic('\t');
+Real_num_to_PC_no_rounding(Product,twos_expt[2], decimal_places + 1);String_to_PC_Basic("\t\t");
 
 Real_num_to_string_with_rounding(num_string, Product,twos_expt[2], decimal_places);
 
@@ -67,7 +62,8 @@ fpn_result = assemble_fpn (Product,twos_expt[2]);
 
 String_to_PC_Basic ("\t\t");
 
-Serial.print(fpn_result,decimal_places);}
+Serial.print(fpn_result,decimal_places);
+newline_Basic();}
 
 SW_reset;}
 
@@ -85,19 +81,15 @@ long Int_multiplier_24_bit(long multiplier, long multiplicand){
 
 
   
-  for(int m = 0; m < 24; m++){//Char_to_PC_Basic('A');
-  if  (multiplier & 0x800000 >> (23-m))add_48_bit_nums(multiplicand_high, multiplicand_low, &sum_high, &sum_low);
+  for(int m = 0; m < 24; m++){
+  if  (multiplier & 0x800000 >> (23-m))
+  add_48_bit_nums(multiplicand_high, multiplicand_low, &sum_high, &sum_low);
 
- // print_long_as_binary(sum_low, '\r');
-
-  shift_24_bit_num_one_place_left(multiplicand_high, multiplicand_low, &shifted_high, &shifted_low);
+   shift_24_bit_num_one_place_left(multiplicand_high, multiplicand_low, &shifted_high, &shifted_low);
   multiplicand_high = shifted_high;
  multiplicand_low = shifted_low; 
 }
-//Shft result right until it is 25 bits
-  return sum_high;
-  //}
-  }
+ return sum_high;}
 
 
 
@@ -144,41 +136,3 @@ else return (A&B);}
 
 
 /***********************************************************************************************************************************/
-/*long test_low = 0xB2E8AC;
-long test_high = 0;
-long res_high = 0;
-long res_low = 0;
-
-newline_Basic();
-print_long_as_binary(test_high, '\t'); print_long_as_binary(test_low, '\t'); newline_Basic();
-
-while(1){
-if (waitforkeypress_Basic() == 'x')break;
-shift_24_bit_num_one_place_left(test_high, test_low, &res_high,&res_low);
-test_high = res_high;
-test_low = res_low;
-print_long_as_binary(test_high, '\t'); print_long_as_binary(test_low, '\t'); 
-newline_Basic();
-}*/
-
-/*newline_Basic();
-long A_high = 0xB2E8AC;    print_long_as_binary(A_high, '\t');
-long A_low = 72;         print_long_as_binary(A_low, '\r');
-
-long B_high = 0x12ABCD;    print_long_as_binary(B_high, '\t');
-long B_low = 100;        print_long_as_binary(B_low, '\r');
-
-
-long res_low = 0;
-long res_high = 0;
-
-newline_Basic();
-
-add_48_bit_nums( A_high, A_low,  &res_high, &res_low);
-print_long_as_binary(res_high, '\t');print_long_as_binary(res_low, '\r');
-waitforkeypress_Basic();
-add_48_bit_nums( B_high, B_low,  &res_high, &res_low);
-print_long_as_binary(res_high, '\t');print_long_as_binary(res_low, '\r');
-waitforkeypress_Basic();*/
-
- 

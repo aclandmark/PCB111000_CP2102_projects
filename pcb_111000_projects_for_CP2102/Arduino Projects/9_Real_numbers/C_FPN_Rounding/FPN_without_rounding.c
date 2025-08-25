4587ce369a84e92cb6d70155523aae58a6a4fdf2
aclandmark <mark.osborne15@btinternet.com>
2025-08-25 21:42:23 +0100

    Delete FPN_without_rounding.c

--- pcb_111000_projects_for_CP2102/Arduino Projects/9_Real_numbers/C_FPN_Rounding/FPN_without_rounding.c
+++
@@ -1,8 +0,0 @@
-
-void Char_to_PC_Basic(char);
-void Int_to_PC_Basic(long);
-void real_divide(long, long, long *, long *);
-
-
-
-//Tpye subroutine void Real_num_to_PC_no_rounding(long int_num, int twos_expt, char decimal_places){} here
